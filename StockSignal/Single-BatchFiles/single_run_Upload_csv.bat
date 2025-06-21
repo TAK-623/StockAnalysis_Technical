a0a7785e5d055f6cf043388c001437ba053f4048
TAK-623 <tak.note7120@gmail.com>
2025-06-21 23:20:16 +0900

    feat: バッチファイルのパス変更

---
+++ StockSignal/Single-BatchFiles/single_run_Upload_csv.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Running Upload_csv.py...
+python C:/Users/mount/Git/StockAnalysis_Technical/StockSignal/Upload_csv.py
+
+pause
--- StockSignal/Test-BatchFiles/single-test_run_Upload_csv.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Running Upload_csv.py...
-python ../Upload_csv.py
-
-pause
